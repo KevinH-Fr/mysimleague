83809a1d48068247444732e0664192c9d7670722
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-04-08 19:20:44 +0200

    test sur posts

---
+++ app/javascript/controllers/newrecord_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  static targets = ["newrecordContent", "openButton", "closeButton"]
+
+  connect() {
+    this.newrecordContentTarget.hidden = true
+    this.closeButtonTarget.hidden = true
+    console.log("stiumuls dropdown controller")
+  }
+
+  submitEnd(e) {
+    if (e.detail.success) {
+      this.hideModal()
+    }
+  //  console.log(e.detail.success)
+  }
+
+  hideModal(){
+    this.element.remove()
+  }
+
+  openDropdown() {
+    this.newrecordContentTarget.hidden = false
+    this.openButtonTarget.hidden = true
+    this.closeButtonTarget.hidden = false
+  }
+
+  closeDropdown() {
+    this.newrecordContentTarget.hidden = true
+    this.openButtonTarget.hidden = false
+    this.closeButtonTarget.hidden = true
+  }
+
+}
