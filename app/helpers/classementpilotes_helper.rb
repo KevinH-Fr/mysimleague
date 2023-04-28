f905137f5c396495896a38aca0944c0c746fd53d
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-04-28 20:24:25 +0200

    helper classement avec index

--- app/helpers/classementpilotes_helper.rb
+++ app/helpers/classementpilotes_helper.rb
@@ -1,13 +1,14 @@
 module ClassementpilotesHelper
 
+    # classement avec index et tri
     def points_by_pilote
         @pilotes = filtreDivisionCourante.includes(:resultats)
         @points_by_pilote = {}
-        @pilotes.each do |pilote|
+        @pilotes.each_with_index do |pilote, index|
           score_sum = pilote.resultats.sum(:score)
-          @points_by_pilote[pilote] = score_sum
+          @points_by_pilote[pilote] = { score: score_sum, index: index + 1 }
         end
-        @points_by_pilote.sort_by {|k, v| -v}.to_h
-    end
-
+        @points_by_pilote.sort_by { |_, v| -v[:score] }.to_h
+      end
+      
 end
