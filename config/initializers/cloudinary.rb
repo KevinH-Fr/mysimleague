41f3bf637c0f7de25bde88276871e08816c59221
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-08-19 19:04:26 +0200

    test image locales

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -3,7 +3,12 @@ Cloudinary.config do |config|
     config.cloud_name = 'dukne3lhz'
     config.api_key = ENV['CLOUDINARY_KEY']
     config.api_secret = ENV['CLOUDINARY_SECRET']
-    config.secure = true # Optional, ensures HTTPS URLs
+    config.secure = true
+    config.sign_url = true 
+    config.type = "authenticated"
+
+    
+    # Optional, ensures HTTPS URLs
     # Add more configuration options as needed
   end
   
