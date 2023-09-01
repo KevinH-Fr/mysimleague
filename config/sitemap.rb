f3d3a7fc4dcf68eaa0bbe97242e4df2965b1384d
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-09-02 01:57:55 +0200

    sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,9 +1,36 @@
-require 'rubygems'
-require 'sitemap_generator'
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "https://www.racemeifyoucan.com"
 
-SitemapGenerator::Sitemap.default_host = 'https://racemeifyoucan.com'
 SitemapGenerator::Sitemap.create do
-  add '/home', :changefreq => 'daily', :priority => 0.9
-  add '/contact_us', :changefreq => 'weekly'
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically for you.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  #
+  # Examples:
+  #
+  # Add '/articles'
+  #
+  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add all articles:
+  #
+  #   Article.find_each do |article|
+  #     add article_path(article), :lastmod => article.updated_at
+  #   end
+
+  Circuit.find_each do |circuit|
+    add(circuit_path(circuit),
+      :lastmod => circuit.updated_at,
+      :changefreq => 'weekly',
+      :priority => 0.9
+    )
+  end
+
 end
-SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
