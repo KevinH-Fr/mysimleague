1dea874fd37871b47e3c28494ad3de84ac312e0e
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-09-06 23:34:41 +0200

    countdown event

---
+++ app/javascript/controllers/countdown_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["horaire", "timer"];
+  static values = { target: String };
+
+  connect() {
+    this.updateTimer();
+    this.interval = setInterval(() => this.updateTimer(), 1000);
+  }
+
+  disconnect() {
+    clearInterval(this.interval);
+  }
+
+  updateTimer() {
+
+    const dateStr = this.horaireTarget.textContent; // Access the text content of the span
+    const targetDate = new Date(dateStr);
+    
+    const currentDate = new Date();
+    const timeRemaining = new Date(targetDate - currentDate);
+
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = String(timeRemaining.getUTCHours()).padStart(2, "0");
+    const minutes = String(timeRemaining.getUTCMinutes()).padStart(2, "0");
+    const seconds = String(timeRemaining.getUTCSeconds()).padStart(2, "0");
+
+    this.timerTarget.textContent = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+
+    if (timeRemaining <= 0) {
+      this.timerTarget.textContent = "Finished";
+      clearInterval(this.interval);
+    }
+  }
+}
