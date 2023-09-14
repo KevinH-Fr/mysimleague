b057b98ac0a8d489894ad8efe782946a71710a7b
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-09-15 00:14:56 +0200

    correction des preload et des labels form erreurs

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,8 +10,8 @@ pin "jquery", to: "jquery.min.js" #, preload: true
 pin "jquery_ujs", to: "jquery_ujs.js"#, preload: true
 
 pin "html2canvas", to: "https://ga.jspm.io/npm:html2canvas@1.4.1/dist/html2canvas.js"
-pin "local-time", to: "https://ga.jspm.io/npm:local-time@2.1.0/app/assets/javascripts/local-time.js"
 
+pin "local-time", to: "https://ga.jspm.io/npm:local-time@2.1.0/app/assets/javascripts/local-time.js"
 pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.2.0/dist/chart.js"
 pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
 
