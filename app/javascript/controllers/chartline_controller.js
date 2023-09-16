f37e30cb8f344aa5669be9362903524fc47f9cab
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-09-16 13:46:50 +0200

    fix color input label - chartline compare different dates

--- app/javascript/controllers/chartline_controller.js
+++ app/javascript/controllers/chartline_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
     const datasets = [
       {
         label: userName,
-        data: userResultats.user.map(resultat => resultat.score),
+        data: userResultats.user.map(resultat => ({ x: resultat.event, y: resultat.score })),
         fill: true,
         backgroundColor: 'rgba(75, 100, 192, 0.2)',
         borderColor: 'rgba(75, 100, 192, 1)',
@@ -21,20 +21,41 @@ export default class extends Controller {
       }
     ];
 
+    // Check if user_compare data exists
     if (userResultats.user_compare && userResultats.user_compare.length > 0) {
       const userCompareName = JSON.parse(this.data.get("userCompareName")); // Parse userStats as JSON
+
       datasets.push({
         label: userCompareName,
-        data: userResultats.user_compare.map(resultat => resultat.score),
+        data: userResultats.user_compare.map(resultat => ({ x: resultat.event, y: resultat.score })),
         fill: true,
         backgroundColor: 'rgba(240, 128, 128, 0.2)',
         borderColor: 'rgba(240, 128, 128, 1)',
         borderWidth: 1
       });
+    } else {
+      // Add an empty dataset for user_compare if it doesn't exist or has no elements
+      datasets.push({
+        label: "",
+        data: [],
+        fill: true,
+        backgroundColor: 'rgba(0, 0, 0, 0)', // Set background color to transparent
+        borderColor: 'rgba(0, 0, 0, 0)', // Set border color to transparent
+        borderWidth: 0 // Set border width to 0
+      });
     }
 
+    const combinedDates = [...userResultats.user.map(resultat => resultat.event)]; // Initialize with user dates
+
+    if (userResultats.user_compare && userResultats.user_compare.length > 0) {
+      combinedDates.push(...userResultats.user_compare.map(resultat => resultat.event)); // Combine user_compare dates
+    }
+
+    const uniqueDates = Array.from(new Set(combinedDates)); // Remove duplicate dates
+    uniqueDates.sort(); // Sort the dates in ascending order
+
     const data = {
-      labels: userResultats.user.map(resultat => resultat.event),
+      labels: uniqueDates,
       datasets: datasets
     };
 
@@ -43,30 +64,30 @@ export default class extends Controller {
         x: {
           beginAtZero: true,
           ticks: {
-            color: 'white' // Set the color of the x-axis labels to white
+            color: 'white'
           }
         },
         y: {
           beginAtZero: true,
           ticks: {
-            color: 'white' // Set the color of the y-axis labels to white
+            color: 'white'
           }
         }
       },
       plugins: {
         legend: {
           labels: {
-            color: 'white' // Set the color of the legend labels to white
+            color: 'white'
           }
         }
       },
       tooltips: {
         mode: 'index',
         intersect: false,
-        backgroundColor: 'rgba(0, 0, 0, 0.8)', // Set tooltip background color
-        titleFontColor: 'white', // Set tooltip title text color to white
-        bodyFontColor: 'white', // Set tooltip body text color to white
-        footerFontColor: 'white' // Set tooltip footer text color to white
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        titleFontColor: 'white',
+        bodyFontColor: 'white',
+        footerFontColor: 'white'
       }
     };
 
