bb17451b1d48169e132e4c0a5d8eb1bb4f8d7230
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-09-17 03:04:44 +0200

    gtag stimulus controller avec opti

--- app/javascript/controllers/gtag_controller.js
+++ app/javascript/controllers/gtag_controller.js
@@ -1,12 +1,13 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-   // console.log("call google analytics")
+
+    console.log("call gtag");
     // Load Google Analytics script asynchronously
     const script = document.createElement("script");
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-8LZY8X784Z";
     script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id='G-8LZY8X784Z'";
 
     script.onload = () => {
       // Initialize Google Analytics after the script has loaded
@@ -20,5 +21,17 @@ export default class extends Controller {
 
     // Append the script to the document
     document.head.appendChild(script);
+
+    // Add TurboFrames listener to initialize Google Analytics on page change
+    const isInitialLoad = true;
+    document.addEventListener("turbo:load", () => {
+      if (isInitialLoad) {
+        isInitialLoad = false;
+        return;
+      }
+      gtag("config", "G-8LZY8X784Z", {
+        page_path: window.location.pathname,
+      });
+    });
   }
 }
