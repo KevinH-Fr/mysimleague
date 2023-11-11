e312fbef48315c0de93f755daf7c87b7935fc3f8
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-11-11 10:58:07 +0100

    slideshow landingpage

--- app/helpers/landingpage_helper.rb
+++ app/helpers/landingpage_helper.rb
@@ -1,2 +1,10 @@
 module LandingpageHelper
+    def svg_animation(file_path, alt_text)
+        image_tag("#{file_path}?#{Time.now.to_i}", 
+            width: '100%', height: '100%', alt: alt_text, 
+            class: "reanimate-svg mb-3",
+            style: "max-width: 100%; max-height: 30vh;" )
+    end
+
+
 end
