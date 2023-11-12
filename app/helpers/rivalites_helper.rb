fd42380046eca6ab8d617fd3849e3c90c1d69963
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-11-13 00:19:49 +0100

    nouvelle methode helper rivalite

--- app/helpers/rivalites_helper.rb
+++ app/helpers/rivalites_helper.rb
@@ -39,44 +39,50 @@ module RivalitesHelper
       # Initialize points for each pilote for the current event
       pilote1_points = 0
       pilote2_points = 0
-
+    
       pilote1_result = Resultat.find_by(association_user: pilote1.id, event: current_event)
       pilote2_result = Resultat.find_by(association_user: pilote2.id, event: current_event)
-
+    
       # Check if both results are present
       if pilote1_result && pilote2_result
         pilote1_position_course = pilote1_result.course
         pilote2_position_course = pilote2_result.course
-
+    
         pilote1_position_qualif = pilote1_result.qualification
         pilote2_position_qualif = pilote2_result.qualification
-
-        #
-
-        if pilote1_result.dns == true
-          pilote2_points += 1 if pilote2_result.dnf == false && pilote2_result.dns == false
-        elsif pilote2_result.dns == true 
-          pilote1_points += 1 if pilote1_result.dnf == false  && pilote1_result.dns == false
-        elsif pilote1_result.dns == false && pilote2_result.dns == false 
-
-          # Update points based on positions for the current event
-
-          # si aucun pilote dnf 
-          if pilote1_result.dnf == false && pilote2_result.dnf == false 
-            pilote1_points += 1 if pilote1_position_course < pilote2_position_course 
-            pilote2_points += 1 if pilote2_position_course < pilote1_position_course 
+    
+        if pilote1_result.dns && pilote2_result.dns
+          # If both pilots are dns, no points
+        elsif pilote1_result.dns
+          # If pilote1 is dns, only one point for pilote2 if not dnf
+          pilote2_points += 1 if pilote2_result.dnf == false
+        elsif pilote2_result.dns
+          # If pilote2 is dns, only one point for pilote1 if not dnf
+          pilote1_points += 1 if pilote1_result.dnf == false
+        elsif pilote1_result.dnf && pilote2_result.dnf
+          # If both pilots are dnf, give only one point for the one in front in qualification
+          if pilote1_position_qualif < pilote2_position_qualif
+            pilote1_points += 1
+          elsif pilote2_position_qualif < pilote1_position_qualif
+            pilote2_points += 1
           end
-            pilote1_points += 1 if pilote1_position_qualif < pilote2_position_qualif 
-            pilote2_points += 1 if pilote2_position_qualif < pilote1_position_qualif 
+        else
+          # Update points based on positions for the current event
+          pilote1_points += 1 if pilote1_position_course < pilote2_position_course
+          pilote2_points += 1 if pilote2_position_course < pilote1_position_course
+    
+          pilote1_points += 1 if pilote1_position_qualif < pilote2_position_qualif
+          pilote2_points += 1 if pilote2_position_qualif < pilote1_position_qualif
         end
       end
-
+    
       # Accumulate points for each pilote across all events
       cumulative_scores[:pilote1] += pilote1_points
       cumulative_scores[:pilote2] += pilote2_points
-
+    
       scores_by_event << { event: current_event.numero, pilote1: pilote1_points, pilote2: pilote2_points }
     end
+    
 
     leading_pilote_id = cumulative_scores[:pilote1] > cumulative_scores[:pilote2] ? pilote1.id : cumulative_scores[:pilote1] < cumulative_scores[:pilote2] ? pilote2.id : nil
 
