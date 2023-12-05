7e61db7f5681c9e1810bfdff39172d4098b9cc8e
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-12-05 11:32:13 +0100

    gestion nil sur icon abonne

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -5,25 +5,23 @@ module PurchasesHelper
     end
 
     def user_paid_purchases_icon(user)
-      best_purchase = user.purchases.where(status: "paid").max_by { |purchase| purchase.article.niveau }
-      
-      if best_purchase  
-        niveau = best_purchase.article.niveau 
-        if niveau == 1 
-          color = "brown"
-        elsif niveau == 2
-          color = "grey"
-        elsif niveau == 3 
-          color = "yellow"
-        else 
-          color = "blue"
-        end
-
-        content_tag(:span, "#{best_purchase.article.titre}", class: "badge") do 
-          content_tag(:i, "", class: "fa fa-2xl fa-crown mx-0", style: "color: #{color}")
+      best_purchase = user.purchases.where(status: "paid").max_by { |purchase| purchase.article.niveau.to_i }
+    
+      if best_purchase
+        niveau = best_purchase.article.niveau
+        color = case niveau
+                when 1 then "brown"
+                when 2 then "grey"
+                when 3 then "yellow"
+                else "blue"
+                end
+    
+        content_tag(:span, best_purchase.article.titre, class: "badge") do
+          content_tag(:i, "", class: "fa fa-2x fa-crown mx-0", style: "color: #{color}")
         end
       end
     end
+    
 
 end
 
