8ff27193c7b439a180025bc53df013bbcbad32df
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-12-26 00:32:58 +0100

    turbo stream admin setup tool

--- app/controllers/problem_seconds_controller.rb
+++ app/controllers/problem_seconds_controller.rb
@@ -1,5 +1,6 @@
 class ProblemSecondsController < InheritedResources::Base
 
+  before_action :set_problem_second, only: %i[ show edit update destroy ]
   before_action :authorize_admin, only: %i[ new create edit update index destroy ]
 
   def create
@@ -19,8 +20,53 @@ class ProblemSecondsController < InheritedResources::Base
     end
   end
 
+  def edit
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update(@problem_second, partial: 'problem_seconds/form', locals: { problem_second: @problem_second })
+      end
+    end
+  end
+
+  def update
+    
+    respond_to do |format|
+      if @problem_second.update(problem_second_params)
+        
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(
+              @problem_second, 
+              partial: 'problem_seconds/problem_second', 
+              locals: { problem_second: @problem_second }
+            )
+          ]
+         
+        end
+
+        format.html { redirect_to problem_second_url(@problem_second), notice: "problem_second setup was successfully updated." }
+        format.json { render :show, status: :ok, location: @problem_second }
+      else
+
+        format.turbo_stream do 
+          render turbo_stream: turbo_stream.update(@problem_second,
+                                                   partial: "problem_seconds/form",
+                                                   locals: {problem_second: @problem_second})
+        end
+        
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @problem_second.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
+    def set_problem_second
+      @problem_second = ProblemSecond.find(params[:id])
+    end
+    
     def problem_second_params
       params.require(:problem_second).permit(:problem_id, :nom)
     end
--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,9 +1,89 @@
 class ProblemsController < InheritedResources::Base
 
+  before_action :set_problem, only: %i[ show edit update destroy ]
   before_action :authorize_admin, only: %i[ new create edit update index destroy ]
+
+
+  def create
+    @problem = Problem.new(problem_params)
+
+    respond_to do |format|
+      if @problem.save
+
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.append('problems', partial: 'problems/problem', locals: { problem: @problem }),
+            turbo_stream.remove('new_problem'),
+
+            turbo_stream.update(
+              @problem, 
+              partial: 'problems/problem', 
+              locals: { problem: @problem }
+            ) 
+          ]
+        end 
+
+        format.html { redirect_to problem_url(@problem), notice: "problem setup was successfully created." }
+        format.json { render :show, status: :created, location: @problem }
+      else
+
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update('new_problem', partial: 'problems/form', locals: { problem: @problem})
+        end
+
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @problem.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update(@problem, partial: 'problems/form', locals: { problem: @problem })
+      end
+    end
+  end
+
+  def update
+    
+    respond_to do |format|
+      if @problem.update(problem_params)
+        
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update(
+              @problem, 
+              partial: 'problems/problem', 
+              locals: { problem: @problem }
+            )
+          ]
+         
+        end
+
+        format.html { redirect_to problem_url(@problem), notice: "problem setup was successfully updated." }
+        format.json { render :show, status: :ok, location: @problem }
+      else
+
+        format.turbo_stream do 
+          render turbo_stream: turbo_stream.update(@problem,
+                                                   partial: "problems/form",
+                                                   locals: {problem: @problem})
+        end
+        
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @problem.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   private
 
+    def set_problem
+      @problem = Problem.find(params[:id])
+    end
+    
     def problem_params
       params.require(:problem).permit(:nom)
     end
