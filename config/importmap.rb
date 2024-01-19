cf4299448d8f028c2bf5ab25df80fb38d9047a5a
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-01-19 22:44:51 +0100

    preload false on jquery

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,8 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 
-pin "jquery", to: "jquery.min.js", preload: true
-pin "jquery_ujs", to: "jquery_ujs.js", preload: true
+pin "jquery", to: "jquery.min.js", preload: false
+pin "jquery_ujs", to: "jquery_ujs.js", preload: false
 
 pin "html2canvas", to: "https://ga.jspm.io/npm:html2canvas@1.4.1/dist/html2canvas.js", preload: false
 
