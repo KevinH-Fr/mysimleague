c9f22e17687ca5dac1723945e6e136763532fff0
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-02-07 21:03:34 +0100

    tailles icons - espace n° events

--- app/controllers/landingpage_controller.rb
+++ app/controllers/landingpage_controller.rb
@@ -1,7 +1,7 @@
 class LandingpageController < ApplicationController
   def index
     @nb_users = User.all.count
-    @nb_courses = Event.all.count
+    @nb_courses = Event.where('horaire < ?', Time.now).count
     @nb_paris = Pari.all.count
   end
 
