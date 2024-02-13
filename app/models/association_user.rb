b0b787d58ab0241bba7ca12c8346b1ba2c586b71
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-02-13 14:48:27 +0100

    scroll overflow x hidden - controle existence meme user et division avant creation asso user

--- app/models/association_user.rb
+++ app/models/association_user.rb
@@ -17,8 +17,10 @@ class AssociationUser < ApplicationRecord
   validates :user_id, presence: true
   validates :equipe_id, presence: true
 
-  before_create :verif_doublon_user
-  before_update :verif_doublon_user
+  #before_create :verif_doublon_user
+  #before_update :verif_doublon_user
+
+  validate :unique_user_and_division, on: :create
 
   def self.ransackable_attributes(auth_object = nil)
     ["actif", "admin", "created_at", "division_id", "equipe_id", "id", "ligue_id", "pilote", "updated_at", "user_id", "valide"]
@@ -35,26 +37,37 @@ class AssociationUser < ApplicationRecord
   
   private
 
-  # passer les autres asso user de ce user inactif pour garder la courante 
-  def verif_doublon_user
-    existing_active_association_user = AssociationUser.find_by(
+  def unique_user_and_division
+    existing_association_user = AssociationUser.find_by(
       user_id: self.user_id,
-      division_id: self.division_id,
-      actif: true
+      division_id: self.division_id
     )
+
+    if existing_association_user && existing_association_user.id != self.id
+      errors.add(:base, "AssociationUser with the same user and division already exists.")
+    end
+  end
+
+  # passer les autres asso user de ce user inactif pour garder la courante 
+  #def verif_doublon_user
+  #  existing_active_association_user = AssociationUser.find_by(
+  #    user_id: self.user_id,
+  #    division_id: self.division_id,
+  #    actif: true
+  #  )
     
     # si meme user deja dans div et actif et asso courante actif et valide
     # passer les autres passifs pour ne pas avoir de doublons actifs
-    if existing_active_association_user && self.actif && self.valide
+  #  if existing_active_association_user && self.actif && self.valide#
 
-      puts "_______________existing active asso user with same use: #{existing_active_association_user}"
-      
-      AssociationUser.where(
-        user_id: existing_active_association_user.user_id,
-        division_id: existing_active_association_user.division_id
-        ).update_all(actif: false)
-        
-    end
-  end
+#      puts "_______________existing active asso user with same use: #{existing_active_association_user}"
+#      
+#      AssociationUser.where(
+#        user_id: existing_active_association_user.user_id,
+#        division_id: existing_active_association_user.division_id
+#        ).update_all(actif: false)
+#        
+#    end
+#  end
   
 end
