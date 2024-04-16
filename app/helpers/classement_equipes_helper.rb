5cf3d9116997b5b195ce04a4bdf254fafb65747f
KevinH-Fr <kevin.hoffman.france@gmail.com>
2024-04-16 17:52:40 +0200

    modif classement equipe pour avoir equipe que si au moins un pilote est actif et valide dans cette equipe

--- app/helpers/classement_equipes_helper.rb
+++ app/helpers/classement_equipes_helper.rb
@@ -5,27 +5,34 @@ module ClassementEquipesHelper
       equipe_scores = {}
     
       if event.present?
-        resultats = Resultat.includes(association_user: :equipe).joins(event: { division: :saison })
-                                .where("events.numero <= ?", event.numero)
-                                .where("divisions.id = ?", event.division_id)
-                               
+        resultats = Resultat.includes(association_user: :equipe)
+                             .joins(event: { division: :saison })
+                             .where("events.numero <= ?", event.numero)
+                             .where("divisions.id = ?", event.division_id)
     
         resultats.each do |resultat|
-          equipe = resultat.association_user.equipe
+          association_user = resultat.association_user
+          equipe = association_user.equipe if association_user.actif? && association_user.valide?
+          next unless equipe # Skip if no active and valid association_user
+    
           equipe_scores[equipe.id] ||= { score_sum: 0, equipe_nom: equipe.nom }
           equipe_scores[equipe.id][:score_sum] += resultat.score
         end
       end
     
       ranked_equipes = equipe_scores.sort_by { |_equipe_id, data| -data[:score_sum] }
-                                    .map.with_index(1) { |(equipe_id, data), rank| { 
-                                      equipe_id: equipe_id, 
-                                      nom: data[:equipe_nom], 
-                                      score_sum: data[:score_sum], 
-                                      rank: rank } }
+                                    .map.with_index(1) do |(equipe_id, data), rank|
+                                      {
+                                        equipe_id: equipe_id,
+                                        nom: data[:equipe_nom],
+                                        score_sum: data[:score_sum],
+                                        rank: rank
+                                      }
+                                    end
     
       ranked_equipes
     end
+    
     
   
       def compare_equipe_ranks(previous_event, current_event)
